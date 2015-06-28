0e4f57ebcc38435cc2c37527fc7a3156bc43584b
ei12159 <ei12159@fe.up.pt>
2015-06-28 13:50:53 +0100

    first commit - minato 28 added

---
+++ NarutoJustBattle_Unity/Assets/Scripts/movement/CharacterControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+	public class CharacterControl : MonoBehaviour {
+		private CharacterMove m_Character;
+		private bool m_Jump;
+		
+		
+		private void Awake()
+		{
+			m_Character = GetComponent<CharacterMove>();
+		}
+		
+		
+		private void Update()
+		{
+			if (!m_Jump)
+			{
+				// Read the jump input in Update so button presses aren't missed.
+			m_Jump = Input.GetKeyDown(KeyCode.UpArrow);
+		}
+		}
+		
+		
+		private void FixedUpdate()
+		{
+			// Read the inputs.
+			float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			// Pass all parameters to the character control script.
+			m_Character.Move(h, m_Jump);
+			m_Jump = false;
+		}
+	}
---
+++ NarutoJustBattle_Unity/Assets/Scripts/movement/CharacterMove.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour {
+	[SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+	[SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
+	[SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
+	
+	public Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
+	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	private bool m_Grounded;            // Whether or not the player is grounded.
+	public Animator m_Anim;            // Reference to the player's animator component.
+	private Rigidbody2D m_Rigidbody2D;
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	
+	private void Awake()
+	{
+		// Setting up references.
+		m_Anim = GetComponent<Animator>();
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	private void FixedUpdate()
+	{
+		
+		//check if we are on the ground
+		m_Grounded = Physics2D.Linecast(transform.position, m_GroundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+		
+		m_Anim.SetBool("ground", m_Grounded);
+		
+		// Set the vertical animation
+		m_Anim.SetFloat("speed", m_Rigidbody2D.velocity.y);
+	}
+	
+	
+	public void Move(float move, bool jump)
+	{
+		
+		//only control the player if grounded or airControl is turned on
+		if (m_Grounded || m_AirControl)
+		{
+			// Reduce the speed if crouching by the crouchSpeed multiplier
+			//move = (move);
+			
+			// The Speed animator parameter is set to the absolute value of the horizontal input.
+			m_Anim.SetFloat("speed", Mathf.Abs(move));
+			
+			// Move the character
+			m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
+			
+			// If the input is moving the player right and the player is facing left...
+			if (move > 0 && !m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+			// Otherwise if the input is moving the player left and the player is facing right...
+			else if (move < 0 && m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+		}
+		// If the player should jump...
+		if (m_Grounded && jump && m_Anim.GetBool("ground"))
+		{
+			// Add a vertical force to the player.
+			m_Grounded = false;
+			m_Anim.SetBool("ground", false);
+			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+		}
+	}
+	
+	
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+		
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
